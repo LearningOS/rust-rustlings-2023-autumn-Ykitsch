b42b40f5b93e6402e40e74f844fb8f3416404e7a
simon <mockingyang@gmail.com>
2023-10-19 00:20:34 +0800

    function

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(a:i32)->i32 {
+    let mut apple:i32 =2;
+    if a >40 {
+        apple=1;
+    };
+    a*apple
+}
 
 // Don't modify this function!
 #[test]
